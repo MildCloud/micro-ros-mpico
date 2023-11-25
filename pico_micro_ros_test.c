f45ef154330f847ced6540412bd14eb3f756526e
tianshu-pi <tianshuh@umich.edu>
2023-11-25 17:54:40 -0500

    testmotor

---
+++ pico_micro_ros_test.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <std_msgs/msg/int32.h>
+#include <rmw_microros/rmw_microros.h>
+#include <geometry_msgs/msg/twist.h>
+#include <hardware/adc.h>
+#include <rc/motor/motor.h>
+
+
+#include "pico/stdlib.h"
+#include "pico_uart_transports.h"
+
+const uint LED_PIN = 25;
+
+rcl_publisher_t publisher;
+std_msgs__msg__Int32 msg;
+
+rcl_subscription_t subscriber;
+geometry_msgs__msg__Twist sub_msg;
+
+void twist_callback(const void *msg_in) {
+    const geometry_msgs__msg__Twist *twist_msg = (const geometry_msgs__msg__Twist *)msg_in;
+    float linear_x = twist_msg->linear.x;
+    float angular_z = twist_msg->angular.z;
+    printf("recv spped(%f,%f)\n", linear_x, angular_z);
+    adc_init();
+    adc_gpio_init(26);
+    adc_gpio_init(27);
+    adc_gpio_init(28);
+    rc_motor_init();
+    if (linear_x > 0 && angular_z == 0) {
+        gpio_put(LED_PIN, 1);
+        for (int i = 0; i < 3; i++) {
+            rc_motor_set(3, 10000);
+            rc_motor_set(1, 10000);
+            sleep_ms(4);
+        }
+    }
+}
+
+
+int main()
+{
+    rmw_uros_set_custom_transport(
+		true,
+		NULL,
+		pico_serial_transport_open,
+		pico_serial_transport_close,
+		pico_serial_transport_write,
+		pico_serial_transport_read
+	);
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    rcl_timer_t timer;
+    rcl_node_t node;
+    rcl_node_t motor_node;
+    rcl_allocator_t allocator;
+    rclc_support_t support;
+    rclc_executor_t executor;
+
+    allocator = rcl_get_default_allocator();
+
+    const int timeout_ms = 1000; 
+    const uint8_t attempts = 120;
+
+    rcl_ret_t ret = rmw_uros_ping_agent(timeout_ms, attempts);
+
+    if (ret != RCL_RET_OK)
+    {
+        // Unreachable agent, exiting program.
+        return ret;
+    }
+
+    rclc_support_init(&support, 0, NULL, &allocator);
+
+    rclc_node_init_default(&node, "pico_motor_node", "", &support);
+    rclc_subscription_init_default(
+        &subscriber,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+        "/cmd_vel");
+
+    rclc_executor_init(&executor, &support.context, 1, &allocator);
+    rclc_executor_add_subscription(&executor, &subscriber, &sub_msg, &twist_callback, ON_NEW_DATA);
+    while (true)
+    {
+        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+    }
+    // motor_subscriber_spin();
+    return 0;
+}
