ecff3b3e047fc6d0df09d8ee8ccd585b5ececd9a
tianshu-pi <tianshuh@umich.edu>
2023-11-25 21:31:08 -0500

    add publisher and subscriber in the same node

--- pico_micro_ros_test.c
+++ pico_micro_ros_test.c
@@ -17,7 +17,7 @@
 const uint LED_PIN = 25;
 
 rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
+std_msgs__msg__Int32 pico_publish_msg;
 
 rcl_subscription_t subscriber;
 geometry_msgs__msg__Twist sub_msg;
@@ -34,14 +34,20 @@ void twist_callback(const void *msg_in) {
     rc_motor_init();
     if (linear_x > 0 && angular_z == 0) {
         gpio_put(LED_PIN, 1);
-        for (int i = 0; i < 3; i++) {
-            rc_motor_set(3, 10000);
-            rc_motor_set(1, 10000);
-            sleep_ms(4);
-        }
+        rc_motor_set(3, 10000);
+        rc_motor_set(1, -10000);
+        sleep_ms(400);
+        rc_motor_set(3, 0);
+        rc_motor_set(1, 0);
+        gpio_put(LED_PIN, 0);
     }
 }
 
+void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
+{
+    rcl_ret_t ret = rcl_publish(&publisher, &pico_publish_msg, NULL);
+    pico_publish_msg.data++;
+}
 
 int main()
 {
@@ -80,18 +86,39 @@ int main()
     rclc_support_init(&support, 0, NULL, &allocator);
 
     rclc_node_init_default(&node, "pico_motor_node", "", &support);
+
+    // init subscription
     rclc_subscription_init_default(
         &subscriber,
         &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
         "/cmd_vel");
 
-    rclc_executor_init(&executor, &support.context, 1, &allocator);
+    rclc_executor_init(&executor, &support.context, 2, &allocator);
     rclc_executor_add_subscription(&executor, &subscriber, &sub_msg, &twist_callback, ON_NEW_DATA);
+    
+    // init publisher
+    rclc_publisher_init_default(
+			&publisher,
+			&node,
+			ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+			"pico_publisher");
+
+    // init timer
+    rclc_timer_init_default(
+		&timer,
+		&support,
+		RCL_MS_TO_NS(1000),
+		timer_callback);
+
+    rclc_executor_add_timer(&executor, &timer);
+    
+    // spin node some
+    pico_publish_msg.data = 0;
     while (true)
     {
         rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
     }
-    // motor_subscriber_spin();
+
     return 0;
 }
