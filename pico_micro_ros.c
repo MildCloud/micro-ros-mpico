85b470fa3f901847f00b448359bc484f168d1331
tianshu-pi <tianshuh@umich.edu>
2023-11-26 15:40:15 -0500

    publish velocity and distance

--- pico_micro_ros.c
+++ pico_micro_ros.c
@@ -5,7 +5,7 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 #include <std_msgs/msg/int32.h>
-#include <std_msgs/msg/int32_multi_array.h>
+#include <std_msgs/msg/float32_multi_array.h>
 #include <rmw_microros/rmw_microros.h>
 #include <geometry_msgs/msg/twist.h>
 
@@ -14,15 +14,18 @@
 #include <rc/motor/motor.h>
 #include <rc/encoder/encoder.h>
 
-
 #include "pico/stdlib.h"
 #include "pico_uart_transports.h"
 
+#include "pico_micro_ros.h"
+
 const uint LED_PIN = 25;
+
+float enc2meters = ((2.0 * PI * WHEEL_RADIUS) / (GEAR_RATIO * ENCODER_RES));
 
 rcl_publisher_t publisher;
 std_msgs__msg__Int32 pico_publish_msg;
-std_msgs__msg__Int32MultiArray int_array_msg;
+std_msgs__msg__Float32MultiArray array_msg;
 
 rcl_subscription_t subscriber;
 geometry_msgs__msg__Twist sub_msg;
@@ -31,30 +34,29 @@ void twist_callback(const void *msg_in) {
     const geometry_msgs__msg__Twist *twist_msg = (const geometry_msgs__msg__Twist *)msg_in;
     float linear_x = twist_msg->linear.x;
     float angular_z = twist_msg->angular.z;
-    printf("recv spped(%f,%f)\n", linear_x, angular_z);
-    adc_init();
-    adc_gpio_init(26);
-    adc_gpio_init(27);
-    adc_gpio_init(28);
-    rc_motor_init();
+
     if (linear_x > 0 && angular_z == 0) {
         gpio_put(LED_PIN, 1);
-        rc_motor_set(3, 10000);
-        rc_motor_set(1, -10000);
+        // set the left motor
+        rc_motor_set(1, LEFT_MOTOR_POL * 10000);
+        // set the right motor
+        rc_motor_set(3, RIGHT_MOTOR_POL * 10000);
         sleep_ms(400);
-        rc_motor_set(3, 0);
         rc_motor_set(1, 0);
+        rc_motor_set(3, 0);
         gpio_put(LED_PIN, 0);
     }
 }
 
 void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 {
-    int_array_msg.data.data[0] = rc_encoder_read_delta(1);
-    int_array_msg.data.data[1] = rc_encoder_read_delta(3);
-    int_array_msg.data.data[2] = rc_encoder_read_count(1);
-    int_array_msg.data.data[3] = rc_encoder_read_count(3);
-    rcl_ret_t ret = rcl_publish(&publisher, &int_array_msg, NULL);
+    // left velocity and total distance
+    array_msg.data.data[0] = LEFT_ENC_POL * enc2meters * rc_encoder_read_delta(1);
+    array_msg.data.data[2] = LEFT_ENC_POL * enc2meters * rc_encoder_read_count(1);
+    // right velocity and total distance
+    array_msg.data.data[1] = RIGHT_ENC_POL * enc2meters * rc_encoder_read_delta(3);
+    array_msg.data.data[3] = RIGHT_ENC_POL * enc2meters * rc_encoder_read_count(3);
+    rcl_ret_t ret = rcl_publish(&publisher, &array_msg, NULL);
     // pico_publish_msg.data++;
 }
 
@@ -71,6 +73,12 @@ int main()
 
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+    rc_encoder_init();
+    adc_init();
+    adc_gpio_init(26);
+    adc_gpio_init(27);
+    adc_gpio_init(28);
+    rc_motor_init();
 
     rcl_timer_t timer;
     rcl_node_t node;
@@ -110,7 +118,7 @@ int main()
     rclc_publisher_init_default(
 			&publisher,
 			&node,
-			ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32MultiArray),
+			ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray),
 			"pico_publisher");
 
     // init timer
@@ -124,13 +132,12 @@ int main()
     
 
     // spin node some
-    rc_encoder_init();
 
     pico_publish_msg.data = 0;
 
-    int_array_msg.data.capacity = 5;
-    int_array_msg.data.size = 4;
-    int_array_msg.data.data = (int32_t *)malloc(sizeof(int32_t) * int_array_msg.data.capacity);
+    array_msg.data.capacity = 5;
+    array_msg.data.size = 4;
+    array_msg.data.data = (float *)malloc(sizeof(float) * array_msg.data.capacity);
 
     while (true)
     {
---
+++ pico_micro_ros.h
@@ -0,0 +1,16 @@
+#ifndef PICO_MICRO_ROS_H
+#define PICO_MICRO_ROS_H
+
+#define WHEEL_RADIUS 0.042
+#define GEAR_RATIO 78.0
+#define ENCODER_RES 20.0
+#define WHEEL_BASE 0.15
+
+#define LEFT_ENC_POL 1
+#define RIGHT_ENC_POL -1
+#define LEFT_MOTOR_POL -1
+#define RIGHT_MOTOR_POL 1
+
+#define PI 3.142
+
+#endif
